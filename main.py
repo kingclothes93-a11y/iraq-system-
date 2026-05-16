9bf8026e263c5623ff4389587bd6015150f00952
kingclothes93-a11y <kingclothes93@gmail.com>
2026-05-16 03:37:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-
-class MainLayout(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = "vertical"
-
-        self.add_widget(Label(text="Iraq System 🇮🇶"))
-        self.add_widget(Label(text="App is working successfully"))
 
 class IraqApp(App):
     def build(self):
-        return MainLayout()
+        return Label(text="Iraq System 🇮🇶")
 
 if __name__ == "__main__":
     IraqApp().run()
